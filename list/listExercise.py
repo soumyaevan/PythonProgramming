725738a6cfcf066cc578d7ee45fab827d020fab1
Soumya Sen <sensoumya94@gmail.com>
2020-08-15 11:41:12 +0200

    Added more list exercises

--- list/listExercise.py
+++ list/listExercise.py
@@ -12,10 +12,27 @@
 # Add the string "Done" to the beginning of the list
 
 # Run the tests to make sure you've done this correctly!
+#
+# instructors = []
+# instructors.extend(["Colt","Blue","Lisa"])
+# instructors.pop()
+# instructors.pop(0)
+# instructors.insert(0,"Done")
+# # print(instructors[::-1])
+#
+#
+# a = [5,2,6,1,4,8,6,1,4]
+# print(a.index(min(a)))
 
-instructors = []
-instructors.extend(["Colt","Blue","Lisa"])
-instructors.pop()
-instructors.pop(0)
-instructors.insert(0,"Done")
-# print(instructors[::-1])
+
+t = int(input())
+
+for _ in range(t):
+    n , k = map(int , input().split())
+    print(n)
+    print(k)
+    print("*********")
+    print(type(n))
+    # print(k-1 if ((k-1) | k) <= n else k-2)
+# print(n)
+# print(k)
