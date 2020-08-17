3866287bf4c2f2af1c2beab832e5958fd1f91b61
Soumya Sen <sensoumya94@gmail.com>
2020-08-17 23:52:36 +0200

    Adding code signal practice test

---
+++ CodeSignal/FixMessage.py
@@ -0,0 +1,6 @@
+"""
+Implement a function that will change the very first symbol of the given message to uppercase,
+and make all the other letters lowercase.
+"""
+def fixMessage(message):
+    return message.lower().capitalize()
